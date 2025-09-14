18fc04ae322736b3304af5d3a669ebd8ee9fe907
Hassan Shaikh <hassanshaikh18092004@gmail.com>
2025-09-14 10:29:39 +0000

    round the corners off the dialog box

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -98,9 +98,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: `calc(var(--radius) - 4px)`,
       },
       keyframes: {
         'accordion-down': {
