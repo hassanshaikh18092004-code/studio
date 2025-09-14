d31554aa26b5ab249c37839a201a6670f39d5a94
Hassan Shaikh <hassanshaikh18092004@gmail.com>
2025-09-14 10:28:39 +0000

    change the font style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,40 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Arial', 'sans-serif'],
-        headline: ['Arial', 'sans-serif'],
-        code: ['monospace'],
+        body: [
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        headline: [
+          'ui-serif', 
+          'Georgia', 
+          'Cambria', 
+          '"Times New Roman"', 
+          'Times', 
+          'serif'
+        ],
+        code: [
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace',
+        ],
       },
       colors: {
         background: 'hsl(var(--background))',
